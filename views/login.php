5976a93b1a85fad23dd77bd88b6ddcc3723822f4
Erick22 <52467300+erick22js@users.noreply.github.com>
2023-07-28 11:59:49 -0300

    Add files via upload
    Setup projeto

---
+++ views/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title></title>
+	</head>
+	<body>
+		
+		<center>
+			<fieldset>
+				
+				<form method="post" action="">
+					<h2>Página de login</h2>
+					
+					<br/><br/>
+					
+					<label>Email:</label><br/>
+					<input type="text" name=""/><br/><br/>
+					
+					<label>Senha:</label><br/>
+					<input type="text" name=""/><br/><br/>
+					
+					<input type="submit" name="" value="Logar"/>
+				</form>
+				
+			</fieldset>
+		</center>
+		
+	</body>
+</html>
