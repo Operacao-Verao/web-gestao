4d10692971451d628afb8883d999b27a96b671e4
erick22js <erickconta22@gmail.com>
2023-08-01 12:19:50 -0300

    Guia de utilização de DAO básica

--- actions/tests.php
+++ actions/tests.php
@@ -3,9 +3,25 @@
 	
 	$daofuncionario = new DAOFuncionario($pdo);
 	
-	$funci = $daofuncionario->findSingleById(5);
-	$funci->setEmail("mariabrega@gmail.com");
-	$daofuncionario->update($funci);
-	var_dump($funci);
+	// Exemplo de criação de Entrada na tabela Funcionário
+	$fun = $daofuncionario->insert("Jorel", "jorel@gmail.com", "senha-facil", TIPO_USUARIO_GESTOR);
+	var_dump($fun);
 	
+	// Se houver sido feito a inserção corretamente, o modelo fica disponível por meio da váriavel
+	if ($fun){
+		// Alterando o modelo e atualizando
+		$fun->setSenha("senha-dificil");
+		$daofuncionario->update($fun);
+	}
+	var_dump($fun);
+	
+	// Buscando por entrada de Jorel no banco de dados
+	$fun = $daofuncionario->findSingleById(8);
+	var_dump($fun);
+	
+	// Se for encontrado, o modelo fica disponível por meio da variável
+	if($fun){
+		// Removendo a entrada da tabela
+		//$daofuncionario->remove($fun);
+	}
 ?>
