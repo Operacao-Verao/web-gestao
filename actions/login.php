bbb105e5eab0150de16f24ff9e915f88073c49be
cyber <cyber@tux.bbrouter>
2023-08-14 19:01:24 -0400

    Master Graal das actions

---
+++ actions/login.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+include_once("../actions/conn.php");
+include_once("../models/Funcionario.php");
+include_once("../daos/DAOFuncionario.php");
+
+class LoginAction {
+    private $pdo;
+    private $daoFuncionario;
+
+    public function __construct(PDO $pdo) {
+        $this->pdo = $pdo;
+        $this->daoFuncionario = new DAOFuncionario($this->pdo);
+    }
+
+    public function execute() {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $email = $_POST["email"];
+            $senha = $_POST["senha"];
+
+            if (empty($email) || empty($senha)) {
+                header("Location: login.php?error=Campos não preenchidos");
+                exit();
+            }
+
+            $funcionario = $this->daoFuncionario->findWithLogin($email, $senha);
+
+            if ($funcionario) {
+                $_SESSION["usuario_id"] = $funcionario->getId();
+                $_SESSION["usuario_nome"] = $funcionario->getNome();
+                $_SESSION["usuario_tipo"] = $funcionario->getTipoUsuario();
+
+                header("Location: home.php");
+                exit();
+            } else {
+                header("Location: login.php?error=Login incorreto");
+                exit();
+            }
+        }
+    }
+}
+
+$loginAction = new LoginAction($pdo);
+$loginAction->execute();
+?>
