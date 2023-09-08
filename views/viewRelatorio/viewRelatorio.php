343fd4cf84fa828bee28c31cac8d365b06e5f2a3
cleitinsz <gabriel.playhard10@gmail.com>
2023-09-08 18:06:17 -0300

    viewRelatorio

---
+++ views/viewRelatorio/viewRelatorio.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+
+<head>
+  <link rel="stylesheet" href="./styles.css" />
+  <title>Defesa Civil - Relatório</title>
+</head>
+
+<?php
+require '../../partials/header/header.php';
+?>
+<section class="dash-content">
+  <article>
+    <div class="topRowContent">
+      <div class="item-row">
+        <p class="item-row-title">Nome: </p>
+        <p class="item-row-content">Samantha</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Cidade: </p>
+        <p class="item-row-content">Franco da Rocha</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Telefone: </p>
+        <p class="item-row-content">11 4002-8922</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Data Atendimento: </p>
+        <p class="item-row-content">17/04/2021</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">CEP: </p>
+        <p class="item-row-content">07851-120</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Bairro: </p>
+        <p class="item-row-content">Jonas</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Celular: </p>
+        <p class="item-row-content">Não possui</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Data Geração: </p>
+        <p class="item-row-content">07/04/2021</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">CPF: </p>
+        <p class="item-row-content">52603903920</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Logradouro: </p>
+        <p class="item-row-content">Jonas</p>
+      </div>
+      <div class="item-row">
+        <p class="item-row-title">Nº: </p>
+        <p class="item-row-content">167</p>
+      </div>
+    </div>
+
+    <!-- Só colocar se tiver alguma foto do relatório  -->
+    <img src="" alt="Foto do relatório">
+    <!-- Só colocar se tiver alguma foto do relatório  -->
+  </article>
+  <article>
+    <div class="item-row">
+      <p class="item-row-title">Técnico Responsável - </p>
+      <p class="item-row-content">Jonas</p>
+    </div>
+    <div class="boxContent">
+      <div class="dadosVistoria">
+        <p class="item-column-title">Dados da Vistoria: </p>
+        <div class="item-content">
+          <p class="item-column-content">(X) Desmoronamento</p>
+          <p class="item-column-content">() Escorregamento</p>
+        </div>
+      </div>
+      <div class="gravidade row">
+        <p class="item-row-title">Gravidade: </p>
+        <p class="item-row-content">() Risco</p>
+        <p class="item-row-content">() Desastre</p>
+      </div>
+      <div class="afetados row">
+        <p class="item-row-title">Afetados: </p>
+        <p class="item-row-content">Adultos: 0</p>
+        <p class="item-row-content">Crianças: 0</p>
+        <p class="item-row-content">Idosos: 0</p>
+        <p class="item-row-content">Deficientes: 0</p>
+        <p class="item-row-content">Total: 0</p>
+      </div>
+      <div class="animais row">
+        <p class="item-row-title">Animais: </p>
+        <p class="item-row-content">Cães: 0</p>
+        <p class="item-row-content">Gatos: 0</p>
+        <p class="item-row-content">Aves: 0</p>
+        <p class="item-row-content">Equinos: 0</p>
+        <p class="item-row-content">Total: 0</p>
+      </div>
+      <div class="tipos">
+        <div>
+          <p class="item-column-title">Área Afetada</p>
+          <p class="item-column-content">() Não especificado</p>
+          <p class="item-column-content">() Pública</p>
+          <p class="item-column-content">() Particular</p>
+        </div>
+        <div>
+          <p class="item-column-title">Tipo de Construção</p>
+          <p class="item-column-content">() Não especificado</p>
+          <p class="item-column-content">() Alvenaria</p>
+          <p class="item-column-content">() Madeira</p>
+          <p class="item-column-content">() Mista</p>
+        </div>
+        <div>
+          <p class="item-column-title">Tipo do Talude</p>
+          <p class="item-column-content">() Não especificado</p>
+          <p class="item-column-content">() Natural</p>
+          <p class="item-column-content">() De Corte</p>
+          <p class="item-column-content">() Aterro</p>
+        </div>
+        <div>
+          <p class="item-column-title">Vegetação</p>
+          <p class="item-column-content">() NÃO</p>
+          <p class="item-column-content">() Rasteira</p>
+          <p class="item-column-content">() Árvores</p>
+        </div>
+      </div>
+      <div class="interdicao-danos">
+        <div class="interdicao">
+          <p class="item-row-title">Interdição: </p>
+          <p class="item-row-content">() Não</p>
+          <p class="item-row-content">() Parcial</p>
+          <p class="item-row-content">() Total</p>
+        </div>
+        <div class="danos">
+          <p class="item-row-title">Danos Materiais: </p>
+          <p class="item-row-content">() Parcial</p>
+          <p class="item-row-content">() Total</p>
+        </div>
+      </div>
+      <div class="memorando-oficio-processo">
+        <div>
+          <p class="item-row-title">Memorando: </p>
+          <p class="item-row-content"></p>
+        </div>
+        <div>
+          <p class="item-row-title">Ofício: </p>
+          <p class="item-row-content"></p>
+        </div>
+        <div>
+          <p class="item-row-title">Processo: </p>
+          <p class="item-row-content"></p>
+        </div>
+      </div>
+      <div class="setor-data">
+        <div>
+          <p class="item-row-title">Setor: </p>
+          <p class="item-row-content"></p>
+        </div>
+        <div>
+          <p class="item-row-title">Data: </p>
+          <p class="item-row-content"></p>
+        </div>
+      </div>
+      <div class="assunto">
+        <p class="item-row-title">Assunto: </p>
+        <p class="item-row-content"></p>
+      </div>
+    </div>
+  </article>
+</section>
+</main>
+
+</html>
